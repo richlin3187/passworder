ec0e19fd4528456b3ec798b4a31dd53b8b48525e
Richard <richlin.3187@gmail.com>
2025-02-06 22:10:22 -0500

    Added base components - and basic things like readme, cryptor class and gitignore plus launch.json file.

---
+++ Cryptor.cs
@@ -0,0 +1,46 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public class Cryptor 
+{
+    public string Encrypt(string plainText, string password, string iv)
+    {
+        using MemoryStream ms = new(ToBytes(plainText));
+        using Aes aes = Aes.Create();
+        aes.GenerateIV(); // this is a salt value
+        aes.Key = ToBytes(password);
+        aes.IV = ToBytesHalved(iv);
+
+        // create an encryptor
+        using CryptoStream cse = new(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+        // write the encrypted text to the stream
+        using StreamWriter sw = new(cse);
+        return sw.ToString();
+    }
+    
+    public string Decrypt(string cypherText, string password, string iv)
+    {
+        using MemoryStream ms = new(ToBytes(cypherText));
+        using Aes aes = Aes.Create();
+        // create a decryptor
+        var key = ToBytes(password);
+        using CryptoStream csd = new(ms, aes.CreateDecryptor(ToBytes(password), ToBytesHalved(iv)), CryptoStreamMode.Read);
+
+        using StreamReader sr = new(csd);
+        string decryptedText = sr.ReadToEnd();
+        return decryptedText;
+    }
+
+    private static byte[] ToBytes(string originalValue)
+    {
+        using var sha = SHA256.Create();
+        return sha.ComputeHash(Encoding.ASCII.GetBytes(originalValue));
+    }
+
+    private static byte[] ToBytesHalved(string originalValue)
+    {
+        var fullBytes = ToBytes(originalValue);
+        return [.. fullBytes.Take(16)];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿const string IV = "1234567890";
+Console.WriteLine("Enter a file to encrypt.");
+var fileLocation = Console.ReadLine();
+var fileContents = System.IO.File.ReadAllText(fileLocation!);
+
+Console.WriteLine("Enter a password.");
+var password = Console.ReadLine();
+
+var cryptor = new Cryptor();
+var encryptedText = cryptor.Encrypt(fileContents, password!, IV);
+
+Console.WriteLine(encryptedText);
